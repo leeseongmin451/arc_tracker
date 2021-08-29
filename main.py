4f74ed281cca52251a6b476a432aa0f037173f60
leeseongmin451 <leeseongmin451@gmail.com>
2021-08-29 22:48:44 +0900

    Add basic skeleton code for the game
     - Predefined frequently used colors
     - Screen size, title, game loop, etc.

---
+++ main.py
@@ -0,0 +1,69 @@
+import pygame
+from pygame.locals import *
+
+import ctypes
+ctypes.windll.user32.SetProcessDPIAware()
+
+# START GAME AND INITIAL SETTING#
+pygame.init()
+
+# DEFINING COLORS
+BLACK = (0, 0, 0)
+WHITE1 = (255, 255, 255)
+WHITE2 = (127, 127, 127)
+WHITE3 = (63, 63, 63)
+RED1 = (255, 63, 63)
+RED2 = (127, 31, 31)
+RED3 = (63, 15, 15)
+GREEN1 = (0, 255, 0)
+GREEN2 = (0, 127, 0)
+GREEN3 = (0, 63, 0)
+BLUE1 = (63, 191, 255)
+BLUE2 = (31, 95, 127)
+BLUE3 = (15, 47, 63)
+CYAN1 = (0, 255, 255)
+CYAN2 = (0, 127, 127)
+CYAN3 = (0, 63, 63)
+MAGENTA1 = (255, 0, 255)
+MAGENTA2 = (127, 0, 127)
+MAGENTA3 = (63, 0, 63)
+YELLOW1 = (255, 255, 0)
+YELLOW2 = (127, 127, 0)
+YELLOW3 = (63, 63, 0)
+ORANGE1 = (255, 102, 0)
+ORANGE2 = (127, 51, 0)
+ORANGE3 = (63, 25, 0)
+PURPLE1 = (191, 95, 255)
+PURPLE2 = (95, 47, 127)
+PURPLE3 = (47, 23, 63)
+
+# Set game title
+pygame.display.set_caption("Arc Tracker")
+
+# Create the screen
+screen_width, screen_height = 1920, 1080
+flags = FULLSCREEN | DOUBLEBUF
+screen = pygame.display.set_mode((screen_width, screen_height), flags, 16)
+
+# frame control
+FPS = 60
+fps_clock = pygame.time.Clock()
+
+# Indicates whether continue game loop or break
+running = False
+
+
+# MAIN GAME LOOP #
+
+while running:
+
+
+    screen.fill(BLACK)
+
+    curspos = pygame.mouse.get_pos()
+
+    pygame.display.flip()
+    fps_clock.tick(FPS)
+
+pygame.quit()
+exit()
