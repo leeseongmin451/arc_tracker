7b7ef99a89483de75c241bf1ba14197104f7d416
leeseongmin451 <leeseongmin451@gmail.com>
2021-09-07 23:37:22 +0900

    Split the main.py file
    - main.py file is split into "init.py", "sprites_and_functions.py" and "main.py".
    - "init.py" is for initializing game and basic settings.
    - "sprites_and_functions.py" is for defining all sprites and functions.
    - "main.py" is for actually running game loop.

---
+++ init.py
@@ -0,0 +1,71 @@
+"""
+Initialize the game and basic settings
+"""
+
+
+import pygame
+from pygame.locals import *
+
+import ctypes
+ctypes.windll.user32.SetProcessDPIAware()
+
+# Start game and initial setting
+pygame.init()
+
+# Constants to indicate which kind of input
+LCLICK = 1      # Left click (bool)
+MCLICK = 2      # Middle click (bool)
+RCLICK = 3      # Right click (bool)
+SCRLUP = 4      # Scroll up (bool)
+SCRLDN = 5      # Scroll down (bool)
+CURPOS = 6      # Cursor position (tuple (x, y))
+
+# Defining colors
+BLACK = (0, 0, 0)
+WHITE1 = (255, 255, 255)
+WHITE2 = (127, 127, 127)
+WHITE3 = (63, 63, 63)
+RED1 = (255, 63, 63)
+RED2 = (127, 31, 31)
+RED3 = (63, 15, 15)
+GREEN1 = (0, 255, 0)
+GREEN2 = (0, 127, 0)
+GREEN3 = (0, 63, 0)
+BLUE1 = (63, 191, 255)
+BLUE2 = (31, 95, 127)
+BLUE3 = (15, 47, 63)
+CYAN1 = (0, 255, 255)
+CYAN2 = (0, 127, 127)
+CYAN3 = (0, 63, 63)
+MAGENTA1 = (255, 0, 255)
+MAGENTA2 = (127, 0, 127)
+MAGENTA3 = (63, 0, 63)
+YELLOW1 = (255, 255, 0)
+YELLOW2 = (127, 127, 0)
+YELLOW3 = (63, 63, 0)
+ORANGE1 = (255, 102, 0)
+ORANGE2 = (127, 51, 0)
+ORANGE3 = (63, 25, 0)
+PURPLE1 = (191, 95, 255)
+PURPLE2 = (95, 47, 127)
+PURPLE3 = (47, 23, 63)
+
+# Set game title
+pygame.display.set_caption("Arc Tracker")
+
+# Create the screen
+screen_width, screen_height = 1920, 1080
+flags = FULLSCREEN | DOUBLEBUF
+screen = pygame.display.set_mode((screen_width, screen_height), flags, 16)
+
+# Frame control
+FPS = 60
+fps_clock = pygame.time.Clock()
+
+# Mouse control event dict
+mouse = {LCLICK: False, MCLICK: False, RCLICK: False, SCRLUP: False, SCRLDN: False, CURPOS: (0, 0)}
+
+
+# Loading images
+arc_tracker_img = pygame.image.load("img/character/arc_tracker.png").convert()      # Image of Arc tracker
+arc_tracker_img.set_colorkey(BLACK)                                                 # Remove black region of the image
