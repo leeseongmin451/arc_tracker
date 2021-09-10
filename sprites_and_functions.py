bf99e81098547427fe5f388ea643227814a33527
leeseongmin451 <leeseongmin451@gmail.com>
2021-09-10 21:44:37 +0900

    Add Button class
    - Buttons in this game will be used for starting or quitting game, stage selection, options, etc.

--- sprites_and_functions.py
+++ sprites_and_functions.py
@@ -57,6 +57,141 @@ def collide_with_rect(sprite: pygame.sprite.Sprite, rectgroup: pygame.sprite.Gro
     return any(map(collided, rectgroup))
 
 
+class Button(pygame.sprite.Sprite):
+    """
+    A rectangular button class
+    """
+
+    group = pygame.sprite.Group()       # Button's own sprite group
+
+    def __init__(self, rect, text, text_font, text_font_size, color, default_back_color=(0, 0, 0)):
+        """
+        Initialize and apply basic settings to button
+        :param rect: position and rectangular size of button
+        :param text: text located at the center of button
+        :param text_font: font of text
+        :param text_font_size: font size of text in pixels
+        :param color: color of button boundary and text
+        :param default_back_color: color of background of button, default value is black(0, 0, 0)
+        """
+
+        pygame.sprite.Sprite.__init__(self)
+
+        # Rect attribute
+        self.rect = pygame.Rect(rect)
+
+        # Text & font attribute
+        self.text = text
+        self.text_font = text_font
+        self.text_font_size = text_font_size
+
+        # Color attrubutes
+        self.active_color = color
+        self.font = pygame.font.SysFont(self.text_font, self.text_font_size)
+        self.text_surface = self.font.render(self.text, True, self.active_color)
+        self.text_surface_rect = self.text_surface.get_rect(center=self.rect.center)
+
+        # Background color attributes
+        # Default color is the darkest color
+        self.default_back_color = default_back_color
+        # Hovered color displayed when cursor is in rect has half brightness of active color
+        self.hovered_back_color = ((self.active_color[0] + self.default_back_color[0]) // 2,
+                                   (self.active_color[1] + self.default_back_color[1]) // 2,
+                                   (self.active_color[2] + self.default_back_color[2]) // 2)
+        # Clicked color displayed when the button is pressed has half brightness of hovered color
+        self.clicked_back_color = ((self.hovered_back_color[0] + self.default_back_color[0]) // 2,
+                                   (self.hovered_back_color[1] + self.default_back_color[1]) // 2,
+                                   (self.hovered_back_color[2] + self.default_back_color[2]) // 2)
+        # Set initial color of button
+        self.current_color = self.active_color
+        self.current_back_color = self.default_back_color
+        self.inactive_color = self.hovered_back_color
+
+        # Boolean values for button control
+        self.active = True
+        self.cursor_in_rect = False
+        self.is_clicked = False
+
+        # Add self to button group
+        all_sprites.add(self)
+        self.group.add(self)
+
+    def activate(self):
+        """
+        Make button active(clickable)
+        :return: None
+        """
+
+        self.current_color = self.active_color
+        # Rerender text surface with newly set color
+        self.text_surface = self.font.render(self.text, True, self.active_color)
+        self.text_surface_rect = self.text_surface.get_rect(center=self.rect.center)
+        self.active = True      # Update method will be executed
+
+    def deactivate(self):
+        """
+        Make button inactive(not clickable)
+        :return:
+        """
+
+        self.current_color = self.inactive_color
+        # Rerender text surface with newly set color
+        self.text_surface = self.font.render(self.text, True, self.active_color)
+        self.text_surface_rect = self.text_surface.get_rect(center=self.rect.center)
+        self.active = False     # Update method will be passed
+        self.current_back_color = self.default_back_color
+
+    def update(self, mouse_state, key_state) -> None:
+        """
+        Updating method needed for all sprite class
+
+        Check whether cursor is in button or clicked the button when button is active(clickable).
+        Only if click-and-release the button, operate() method will be executed.
+
+        :param mouse_state: Dictionary of clicking event and position info
+        :param key_state: Dictionary of event from pressing keyboard
+        :return: None
+        """
+
+        # Check whether cursor is in button boundary and change background color
+        if self.active and self.rect.collidepoint(mouse_state[CURPOS]):
+            self.cursor_in_rect = True
+            self.current_back_color = self.hovered_back_color       # Change background status
+        else:
+            self.cursor_in_rect = False
+            self.current_back_color = self.default_back_color       # Change background status
+
+        # Check mouse click event when the cursor is in button
+        if self.cursor_in_rect and mouse_state[LCLICK]:
+            self.is_clicked = True
+            self.current_back_color = self.clicked_back_color       # Change background status
+        # Check mouse release event when clicked
+        if self.is_clicked and not mouse_state[LCLICK]:
+            self.operate()                                          # Operate the button
+            self.is_clicked = False
+            self.current_back_color = self.hovered_back_color       # Change background status
+
+    def operate(self):
+        """
+        Button's specific function will be defined here.
+        Child button classes will have specific functions by overriding this method
+        :return: None
+        """
+
+        pass
+
+    def draw(self, surface):
+        """
+        Draw button on screen
+        :param surface: surface on which draw button
+        :return: None
+        """
+
+        pygame.draw.rect(surface, self.current_back_color, self.rect)   # Draw background first
+        pygame.draw.rect(surface, self.current_color, self.rect, 3)     # Draw boundary of button
+        surface.blit(self.text_surface, self.text_surface_rect)         # Draw text in button
+
+
 class ArcTracker(pygame.sprite.Sprite):
     """
     A sprite controlled by player
