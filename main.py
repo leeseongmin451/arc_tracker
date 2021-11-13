aca8c3da2fb126d4355e9b29d4ef40ef379f4a20
leeseongmin451 <leeseongmin451@gmail.com>
2021-11-13 18:09:28 +0900

    Add comments in main.py

--- main.py
+++ main.py
@@ -21,22 +21,27 @@ while init.running:
     # Get all kind of events generated from keyboard
     keys = pygame.key.get_pressed()
 
+    # Update and draw main menu screen
     if mainmenu_screen.now_display:
         mainmenu_screen.update(mouse, keys)
         mainmenu_screen.draw(screen)
 
+    # Update and draw level selection screen
     elif level_select_screen.now_display:
         level_select_screen.update(mouse, keys)
         level_select_screen.draw(screen)
 
+    # Update and draw how-to-play screen
     elif how_to_play_screen.now_display:
         how_to_play_screen.update(mouse, keys)
         how_to_play_screen.draw(screen)
 
+    # Update and draw settings screen
     elif settings_screen.now_display:
         settings_screen.update(mouse, keys)
         settings_screen.draw(screen)
 
+    # Update and draw gameplay screen
     elif gameplay_screen.now_display:
         gameplay_screen.update(mouse, keys)
         gameplay_screen.draw(screen)
